be570601f1aa6faa5a6919e08461db7fa97609e6
Anthony Canet <tony264269@gmail.com>
2023-11-27 16:59:44 +0100

    exercice5

---
+++ Exercice_5/Exercice_5.js
@@ -0,0 +1,16 @@
+
+let images = document.querySelectorAll('img');
+console.log(images);
+
+
+   
+images.addEventListener('mouseover',function ()
+{
+    for (image = 0; pas < 4; image++) {
+       
+        console.log("image");
+      }
+})
+  
+   
+     
