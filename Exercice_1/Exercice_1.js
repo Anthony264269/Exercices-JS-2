bc2b704bb5f59327056ae2a01d5e6e4ae537d0e1
Anthony Canet <tony264269@gmail.com>
2023-11-27 09:46:59 +0100

    exercice1

---
+++ Exercice_1/Exercice_1.js
@@ -0,0 +1,17 @@
+
+function onHover() {
+    document.getElementById('image1').src='images/image1_2.jpg'
+}
+   
+            
+  
+  function offHover() {
+    document.getElementById('image1').src='images/image1.jpg'
+
+  }
+    
+      
+          
+
+  
+  
